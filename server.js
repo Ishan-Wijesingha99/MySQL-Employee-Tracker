60c99f33617b18b3303fa6dffe5145f1c979a18f
Ishan <ishanwijes@gmail.com>
2022-08-10 00:31:15 +1000

    added loop, async functionality, inquirer prompts

---
+++ server.js
@@ -0,0 +1,168 @@
+
+const inquirer = require('inquirer')
+const mysql = require('mysql2')
+
+let loopRunning = true;
+
+let departmentArray = []
+let employeeArray = []
+let roleArray = []
+
+
+
+const runApplication = async function() {
+
+    while(loopRunning) {
+
+        const p = await inquirer.createPromptModule([
+            {
+                type: 'list',
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
+                message: "What would you like to do",
+                name: 'option'
+            },
+        ])
+
+        if(p.option === 'View All Employees') {
+
+
+
+
+
+
+
+
+
+        } else if(p.option === 'Add Employee') {
+
+            let employeePromiseObject = await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: "What is the employee's first name?",
+                    name: 'employeeFirstName'
+                },
+                {
+                    type: 'input',
+                    message: "What is the employee's last name?",
+                    name: 'employeeLastName'
+                },
+                {
+                    type: 'list',
+                    choices: [...roleArray],
+                    message: "What is the employee's role?",
+                    name: 'employeeRole'
+                },
+                {
+                    type: 'list',
+                    choices: [...employeeArray],
+                    message: "Who is the employee's manager?",
+                    name: 'employeeManager'
+                },
+            ])
+
+
+
+
+
+
+
+
+        } else if(p.option === 'Update Employee Role') {
+            
+            
+
+
+
+
+
+
+
+
+        } else if(p.option === 'View All Roles') {
+            
+            let updateRolePromiseObject = await inquirer.prompt([
+                {
+                    type: 'list',
+                    choices: [employeeArray],
+                    message: "Which employee's role do you want to update?",
+                    name: 'roleUpdateFullName'
+                },
+                {
+                    type: 'list',
+                    choices: [roleArray],
+                    message: "Which role do you want to assign the selected employee?",
+                    name: 'newRole'
+                },
+            ])
+
+
+
+
+
+
+
+        } else if(p.option === 'Add Role') {
+            
+            let rolePromiseObject = await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: "What is the name of the role?",
+                    name: 'roleName'
+                },
+                {
+                    type: 'input',
+                    message: "What is the salary of the role?",
+                    name: 'roleSalary'
+                },
+                {
+                    type: 'input',
+                    message: "What department does the role belong to?",
+                    name: 'roleDepartment'
+                },
+            ])
+
+            // add role based off this
+
+
+
+
+
+
+
+        } else if(p.option === 'View All Departments') {
+            
+
+
+
+
+
+
+
+        } else if(p.option === 'Add Department') {
+            
+            let departmentPromiseObject = await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: "What is the name of the department?",
+                    name: 'departmentName' 
+                },
+            ])
+
+            // add department
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+}
